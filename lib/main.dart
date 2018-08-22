dc756f3b80df8a5673534bb8aa3477b82280de9a
Waleed <waleed_arshad006@hotmail.com>
2018-08-22 19:19:53 +0500

    Add contact selection feature. Fix design.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Custom contact selection'),
     );
   }
 }
@@ -22,13 +22,35 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
+  final String reloadLabel = 'Reload!';
+  final String fireLabel = 'Fire in the hole!';
+  final Color floatingButtonColor = Colors.red;
+  final IconData reloadIcon = Icons.refresh;
+  final IconData fireIcon = Icons.filter_center_focus;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState(
+        floatingButtonLabel: this.fireLabel,
+        icon: this.fireIcon,
+        floatingButtonColor: this.floatingButtonColor,
+      );
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Contact> _contacts = new List<Contact>();
+  List<CustomContact> _uiCustomContacts = List<CustomContact>();
+  List<CustomContact> _allContacts = List<CustomContact>();
+  bool _isLoading = false;
+  bool _isSelectedContactsView = false;
+  String floatingButtonLabel;
+  Color floatingButtonColor;
+  IconData icon;
+
+  _MyHomePageState({
+    this.floatingButtonLabel,
+    this.icon,
+    this.floatingButtonColor,
+  });
 
   @override
   void initState() {
@@ -44,55 +66,119 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: _contacts != null
+      body: !_isLoading
           ? Container(
               child: ListView.builder(
-                itemCount: _contacts?.length,
+                itemCount: _uiCustomContacts?.length,
                 itemBuilder: (BuildContext context, int index) {
-                  Contact c = _contacts[index];
-                  var list = c.phones.toList();
+                  CustomContact _contact = _uiCustomContacts[index];
+                  var _phonesList = _contact.contact.phones.toList();
+
                   return Column(
                     children: <Widget>[
-                      ListTile(
-                        leading: (c.avatar != null)
-                            ? CircleAvatar(
-                                backgroundImage: MemoryImage(c.avatar))
-                            : CircleAvatar(
-                                child: Text(c.displayName[0],
-                                    style: TextStyle(color: Colors.black)),
-                              ),
-                        title: Text(c.displayName ?? ""),
-                        subtitle: list.length >= 1 && list[0]?.value != null
-                            ? Text(list[0].value)
-                            : Text(''),
-                        trailing: Checkbox(value: false, onChanged: (bool) {}),
-                      ),
-                      Divider()
+                      _buildListTile(_contact, _phonesList),
                     ],
                   );
                 },
               ),
             )
-          : Center(child: CircularProgressIndicator()),
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       floatingActionButton: new FloatingActionButton.extended(
-        onPressed: () {},
-        icon: Icon(Icons.graphic_eq),
-        label: Text('Fire in the hole!'),
+        backgroundColor: floatingButtonColor,
+        onPressed: _onSubmit,
+        icon: Icon(icon),
+        label: Text(floatingButtonLabel),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  refreshContacts() async {
-    var contacts = await ContactsService.getContacts();
+  void _onSubmit() {
     setState(() {
-      _contacts = contacts.where((item) => item.displayName != null).toList();
-      _contacts.sort((a, b) => a.displayName.compareTo(b.displayName));
+      if (!_isSelectedContactsView) {
+        _uiCustomContacts =
+            _allContacts.where((contact) => contact.isChecked == true).toList();
+        _isSelectedContactsView = true;
+        _restateFloatingButton(
+          widget.reloadLabel,
+          Icons.refresh,
+          Colors.green,
+        );
+      } else {
+        _uiCustomContacts = _allContacts;
+        _isSelectedContactsView = false;
+        _restateFloatingButton(
+          widget.fireLabel,
+          Icons.filter_center_focus,
+          Colors.red,
+        );
+      }
+    });
+  }
+
+  ListTile _buildListTile(CustomContact c, List<Item> list) {
+    return ListTile(
+      leading: (c.contact.avatar != null)
+          ? CircleAvatar(backgroundImage: MemoryImage(c.contact.avatar))
+          : CircleAvatar(
+              child: Text(
+                  (c.contact.displayName[0] +
+                      c.contact.displayName[1].toUpperCase()),
+                  style: TextStyle(color: Colors.white)),
+            ),
+      title: Text(c.contact.displayName ?? ""),
+      subtitle: list.length >= 1 && list[0]?.value != null
+          ? Text(list[0].value)
+          : Text(''),
+      trailing: Checkbox(
+          activeColor: Colors.green,
+          value: c.isChecked,
+          onChanged: (bool value) {
+            setState(() {
+              c.isChecked = value;
+            });
+          }),
+    );
+  }
+
+  void _restateFloatingButton(String label, IconData icon, Color color) {
+    floatingButtonLabel = label;
+    icon = icon;
+    floatingButtonColor = color;
+  }
+
+  refreshContacts() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var contacts = await ContactsService.getContacts();
+    _populateContacts(contacts);
+  }
+
+  void _populateContacts(Iterable<Contact> contacts) {
+    _contacts = contacts.where((item) => item.displayName != null).toList();
+    _contacts.sort((a, b) => a.displayName.compareTo(b.displayName));
+    _allContacts =
+        _contacts.map((contact) => CustomContact(contact: contact)).toList();
+    setState(() {
+      _uiCustomContacts = _allContacts;
+      _isLoading = false;
     });
   }
 
   void getContactsPermission() {
     SimplePermissions.requestPermission(Permission.ReadContacts);
   }
+}
+
+class CustomContact {
+  final Contact contact;
+  bool isChecked;
+
+  CustomContact({
+    this.contact,
+    this.isChecked = false,
+  });
 }
